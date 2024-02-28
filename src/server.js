dff319b791739db11e6f3bb4df5f56f3383ef294
Minh Huy <tmh.pug@gmail.com>
2024-02-28 23:18:28 +0700

    Add connect to database mysql

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
+const mysql = require("mysql2");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -12,6 +13,20 @@ const hostname = process.env.HOST_NAME;
 configViewEngine(app);
 
 app.use("/", webRoutes);
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3307, // default 3306
+  user: "root", // default password: empty
+  password: "123456",
+  database: "nodejs",
+});
+
+connection.query("SELECT * FROM Persons", function (err, result, fields) {
+  console.log(">>> result =", result);
+  console.log(">>> fields =", fields);
+});
+
 app.listen(port, hostname, () => {
   console.log(`Example app listening on port ${port}`);
 });
