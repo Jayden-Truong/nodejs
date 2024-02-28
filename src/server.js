3fad8cf6a40b5412c729fbd569b0de51af892d5d
Minh Huy <tmh.pug@gmail.com>
2024-02-28 23:32:04 +0700

    Reuse connection db

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require("path");
 const configViewEngine = require("./config/viewEngine");
 const webRoutes = require("./routes/web");
-const mysql = require("mysql2");
+const connection = require("./config/database");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -14,17 +14,8 @@ configViewEngine(app);
 
 app.use("/", webRoutes);
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3307, // default 3306
-  user: "root", // default password: empty
-  password: "123456",
-  database: "nodejs",
-});
-
 connection.query("SELECT * FROM Persons", function (err, result, fields) {
   console.log(">>> result =", result);
-  console.log(">>> fields =", fields);
 });
 
 app.listen(port, hostname, () => {
