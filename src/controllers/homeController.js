a5e3a17a421975c7f5ed313aceeb2961b44bddeb
Minh Huy <tmh.pug@gmail.com>
2024-02-29 21:46:56 +0700

    Add UI navbar for home page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,7 @@
 const pool = require("../config/database");
 
 const getHomepage = (req, res) => {
-  let users = [];
-  pool.query("SELECT * FROM Persons", function (err, result, fields) {
-    users = result;
-    console.log(users);
-    res.send(JSON.stringify(users));
-  });
+  return res.render("home.ejs");
 };
 
 module.exports = {
